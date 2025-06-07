a3730ae77909c0db7b4a8a0cfa9b5d2e9336a625
ricardolopessqill <ricardo.lopes@sqill.so>
2025-06-07 01:34:51 +0100

    add sqill

--- src/app/home/resume/resume.component.ts
+++ src/app/home/resume/resume.component.ts
@@ -19,7 +19,7 @@ export class ResumeComponent implements OnInit, OnChanges {
 
   constructor() {
 
-    const job_1: Job = new Job('C# Developer and Full Stack Developper', 'Since 2021', 'EXAUD',
+    const job_1: Job = new Job('C# Developer and Full Stack Developper', '2021-2024', 'EXAUD',
       '<P>' 
         + '</P><ul><li><a href="https://www.learningdesignnetwork.com/ldn-interactive"><b>Learning Design Network</b></a><P>A virtual lobby with common areas and mini games for Kaiser Permanente workers and patients.</P></il><li><a href="https://twentytwenty.com/pipeline/vs-2020/"><b> VS Headset</b></a><P>Two applications designed to provide diagnostic of a patient\'s eyesight through the use of HTC Vive Pro Eye eye tracking camera.</P></li></ul><li><a href="https://www.testsociety.pt/"><b>TestSociety</b></a><P>A static website promoting a QA Conference.</P></li></ul><li><b>Exaud HR</b><P>An in-house project designed for senior team-members to easily search for all co-workers skills and leaves.</P></li></ul>', 1);
 
@@ -30,13 +30,18 @@ export class ResumeComponent implements OnInit, OnChanges {
     const job_3: Job = new Job('Full-Stack Sofware Developper', 'Summers of 2017 and 2018', 'IPBrick',
       '<P> Implementing new features, code reviewing and unit testing.</p>', 1);
 
+    const job_4: Job = new Job('C# Developer', 'Since 2024', 'Sqill',
+      '<P>' 
+        + '</P><ul><li><a href="https://play.google.com/store/apps/details?id=com.heysqill.enterprise&hl=en"><b>Sqill Go</b></a><P>A social media suite, designed to help both social media managers simplify their workflow and creators editing and fullfilling the manager requests.</P></li></ul>', 1);
+
+
     const school_1: Job = new Job('M.Sc. in Informatics and Computing Engineering', '2012-2021', 'Faculdade de Engenharia da Universidade do Porto',
       '<P></P>', 1);
 
     const school_2: Job = new Job('Level 3 QRQ - Scientific/Tecnological in Informatic', '2010-2012', 'Colégio Internato dos Carvalhos',
       '<P></P>', 1);
 
-    const trabalho_1: Job = new Job('Progamador C# e Full Stack', 'Desde 2021', 'EXAUD',
+    const trabalho_1: Job = new Job('Progamador C# e Full Stack', '2021-2024', 'EXAUD',
       '<P>' 
         + '</P><ul><li><a href="https://www.learningdesignnetwork.com/ldn-interactive"><b>Learning Design Network</b></a><P>Um ambiente virtual com áreas comuns e mini-jogos para funcionários e pacientes da Kaiser Permanente.</P></il><li><a href="https://twentytwenty.com/pipeline/vs-2020/"><b> VS Headset</b></a><P> Duas aplicações desenvolvidades para diagnóstico de condições visuais de pacientes através do uso da câmera de rastreamento ocular do HTC VIVE Pro Eye.</P></li></ul><li><a href="https://www.testsociety.pt/"><b>TestSociety</b></a><P>Um website estático para promover um conferência de QA.</P></li></ul><li><b>Exaud HR</b><P>Um projecto interno para os engenheiros-chefes para procurarem facilmente as habilidades, ferramentas, saídas de escritório e férias dos funcionários.</P></li></ul>', 1);
 
@@ -47,6 +52,11 @@ export class ResumeComponent implements OnInit, OnChanges {
     const trabalho_3: Job = new Job('Programador Full-Stack', 'Verão de 2017 e de 2018', 'IPBrick',
       '<P> Implementar novas funcionalidades, testes unitários e revisão de código.</p>', 1);
 
+    const trabalho_4: Job = new Job('C# Developer', 'Desde 2024', 'Sqill',
+      '<P>' 
+        + '</P><ul><li><a href="https://play.google.com/store/apps/details?id=com.heysqill.enterprise&hl=pt"><b>Sqill Go</b></a><P>Um pacote de ferramentes de gestão de social media desenhado para ajudar tanto os gerentes de social media a simplificar seu fluxo de trabalho como os criadores a editar e a corresponder às solicitações dos gerentes..</P></li></ul>', 1);
+
+
     const escola_1: Job = new Job('Mestrado Integrado em Engenharia Informática e Computação', '2012-2021', 'Faculdade de Engenharia da Universidade do Porto',
       '<P></P>', 1);
 
@@ -55,8 +65,8 @@ export class ResumeComponent implements OnInit, OnChanges {
     
     
     
-    this.jobsEnglish = [job_1,job_2,school_1,job_3,school_2];
-    this.jobsPortuguese = [trabalho_1,trabalho_2,escola_1, trabalho_3,escola_2];
+    this.jobsEnglish = [job_4, job_1,job_2,school_1,job_3,school_2];
+    this.jobsPortuguese = [trabalho_4, trabalho_1,trabalho_2,escola_1, trabalho_3,escola_2];
     this.jobs = this.jobsEnglish;
   }
 
